c1a5ac0aee39365aa9cf444fa6e953eab11b5d04
Midga3 <mikhaylodm@yandex.ru>
2025-10-08 20:33:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def hello_world():
     return 'Hello world!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
